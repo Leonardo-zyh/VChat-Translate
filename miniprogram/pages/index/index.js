c78e4d41b3be1b8d176088f07b7efaa035a056b7
Leonardo_zyh <956731563@qq.com>
2018-10-16 22:18:36 +0800

    VChat翻译配置

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,14 @@
+const app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      '/images/77c6a7efce1b9d162f210013fedeb48f8d5464da.jpg',
+      '/images/f603918fa0ec08faf4f358d454ee3d6d54fbdad6.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  
+})
